ce169f250a825b0aca4e316e5412dd9f0d076bfd
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-15 09:54:35 -0600

    Move admin merchants controller

--- app/controllers/admin/admin_merchants_controller.rb
+++ app/controllers/admin/admin_merchants_controller.rb
@@ -1,4 +1,4 @@
-  class AdminMerchantsController < ApplicationController
+  class Admin::MerchantsController < ApplicationController
     def index
       @merchants = Merchant.all
     end
@@ -14,12 +14,12 @@
     def update
       merchant = Merchant.find(params[:merchant_id])
       if merchant.update(merch_params)
-         redirect_to "/admin/merchants/#{merchant.id}"
-         flash[:success] = 'Merchant Updated'
-       end
-         redirect_to "/admin/merchants/#{merchant.id}/edit"
-         flash[:danger] = 'Merchant Not Updated: re-enter information'
-       end 
+       redirect_to "/admin/merchants/#{merchant.id}"
+       flash[:success] = 'Merchant Updated'
+      else
+       redirect_to "/admin/merchants/#{merchant.id}/edit"
+       flash[:danger] = 'Merchant Not Updated: re-enter information'
+      end
     end
 
     def create
