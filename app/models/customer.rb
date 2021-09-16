e20ccb00450392fbb29cb8944605807ba22dd990
iPowers <isika.powers@gmail.com>
2021-09-16 17:30:39 -0600

    Add top_five_customers to customer model, along with has many invoices and has many transactions through invoices relationships.

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,14 @@
 class Customer < ApplicationRecord
-  has_many :invoices
   validates :first_name, :last_name, :created_at, :updated_at, presence: true
+
+  has_many :invoices
+  has_many :transactions, through: :invoices
+
+  def self.top_five_customers
+    joins(:transactions).where("transactions.result = ?", 1)
+                        .group(:id)
+                        .select("customers.*, count('transactions.result') as trans_count")
+                        .order(trans_count: :desc)
+                        .limit(5)
+  end
 end
