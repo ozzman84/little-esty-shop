4ac704b18aa6e3dfcd2531af6d1ca99851ebaed3
Ida Olson <idaolson@gmail.com>
2021-09-17 14:53:07 -0600

    Add update method to items controller

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,10 +2,22 @@ class ItemsController < ApplicationController
   def index
     @merchant = Merchant.find(params[:merchant_id])
     @merchant_items = @merchant.items
-    pry
   end
 
   def show
     @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+
+    redirect_to merchant_item_path
+    flash[:success] = "Item updated!"
+  end
+
+
+  def item_params
+     params.permit(:name, :description, :unit_price)
   end
 end
