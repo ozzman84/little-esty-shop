5aa9f43a5e198483fae006fcfc17acb201b6dfe3
Ida Olson <idaolson@gmail.com>
2021-09-17 14:52:25 -0600

    Update merchant items show spec

--- spec/features/items/show_spec.rb
+++ spec/features/items/show_spec.rb
@@ -1,32 +1,28 @@
 require 'rails_helper'
 
-RSpec.describe "Merchant item index" do
+RSpec.describe "Merchant show page" do
   before :each do
     @merch1 = Merchant.create!(name: "Douglas Olson")
     @item1 = @merch1.items.create!(name: "Polly Pocket", description: "So pretty", unit_price: 862, created_at: "2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
     @item2 = @merch1.items.create!(name: "Cabbage Patch Doll", description: "Cute", unit_price: 1239, created_at: "2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
     @item3 = @merch1.items.create!(name: "Teddy Ruxpin", description: "Creepy", unit_price: 1543, created_at: "2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
     @item4 = @merch1.items.create!(name: "Barbie", description: "Gorgeous", unit_price: 2183, created_at: "2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
-    @item5 = @merch2.items.create!(name: "GI Joe", description: "Imperialism", unit_price: 743, created_at: "2012-03-27 14:53:59 UTC", updated_at: "2012-03-27 14:53:59 UTC")
   end
 
   it "displays all of an item's attributes" do
-    visit merchant_item_path(@item1.id)
+    visit merchant_item_path(@merch1, @item1)
 
     expect(page).to have_content(@item1.name)
     expect(page).to have_content(@item1.description)
-    expect(page).to have_content(@item1.unit_price)
+    expect(page).to have_content("$8.62")
+  end
+
+  it "has a link to update info" do
+
+    visit merchant_item_path(@merch1, @item1)
+
+    click_link 'Update'
+
+    expect(current_path).to eq(edit_merchant_item_path(@merch1, @item1))
   end
 end
-
-
-
-#
-# As a merchant,
-# When I click on the name of an item from the merchant items index page,
-# Then I am taken to that merchant's item's show page (/merchant/merchant_id/items/item_id)
-# And I see all of the item's attributes including:
-#
-# Name
-# Description
-# Current Selling Price
