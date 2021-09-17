26063d1fcdf8a93be18c7c0194d29ea9d79381a5
iPowers <isika.powers@gmail.com>
2021-09-17 11:14:20 -0600

    Add incomplete invoices method to invoice_items model as well as create its test.

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,7 +1,13 @@
 class InvoiceItem < ApplicationRecord
-  belongs_to :item
-  belongs_to :invoice
   validates :quantity, :unit_price, :status, :created_at, :updated_at, presence: true
 
+  belongs_to :item
+  belongs_to :invoice
+
   enum status: [:pending, :packaged, :shipped]
+
+  def self.incomplete_invoices
+    invoice_ids = InvoiceItem.where("status = 0 OR status = 1").pluck(:invoice_id)
+    Invoice.order(created_at: :asc).find(invoice_ids)
+  end
 end
