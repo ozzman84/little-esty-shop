16d949753776f4510ccb26f9989ef413f6c1f94d
iPowers <isika.powers@gmail.com>
2021-09-18 16:28:55 -0600

    Delete .save and use only .update

--- app/controllers/admin/invoices_controller.rb
+++ app/controllers/admin/invoices_controller.rb
@@ -10,9 +10,9 @@ class Admin::InvoicesController < ApplicationController
   def update
     @invoice = Invoice.find(params[:id])
     @invoice.update(status: params[:status])
-    @invoice.save
 
     redirect_to admin_invoice_path(@invoice)
+    
     flash[:notice] = "Invoice status successfully updated"
   end
 
