8ea4bfd39b4fb3a7af2114ec218fc742886d1090
Ida Olson <idaolson@gmail.com>
2021-09-19 12:32:10 -0600

    Remove timestamp validation tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe Item do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:unit_price) }
-    it { should validate_presence_of(:created_at) }
-    it { should validate_presence_of(:updated_at) }
+    # it { should validate_presence_of(:created_at) }
+    # it { should validate_presence_of(:updated_at) }
   end
 
   describe 'instance methods' do
