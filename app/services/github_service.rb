b6036b5591321c414db96b9780daa5fce517dc75
Rowan DeLong <rowanwinzer@gmail.com>
2021-09-20 16:12:33 -0600

    Add pulls to api stuff

--- app/services/github_service.rb
+++ app/services/github_service.rb
@@ -1,6 +1,20 @@
 class GithubService
-  def get_data
-    response = Faraday.get("https://api.github.com/repos/isikapowers/little-esty-shop")
+
+
+  def get_data(url)
+    response = Faraday.get("https://api.github.com/repos/isikapowers/little-esty-shop#{url}")
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def pulls
+    get_data("/pulls?state=closed&per_page=100")
+  end
+
+  def name
+    get_data("")
+  end
+
+  def get_all
+    result = {name: name, pulls: pulls}
   end
 end
