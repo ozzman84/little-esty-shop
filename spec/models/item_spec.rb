094726a7b35e419a9eb715f550bf3a1e33c2e0ed
Ida Olson <idaolson@gmail.com>
2021-09-21 15:40:23 -0600

    Add test for items best day

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -11,8 +11,6 @@ RSpec.describe Item do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:description) }
     it { should validate_presence_of(:unit_price) }
-    # it { should validate_presence_of(:created_at) }
-    # it { should validate_presence_of(:updated_at) }
   end
 
   describe 'instance methods' do
@@ -49,6 +47,44 @@ RSpec.describe Item do
         @item1.unit_price = 3330
         expect(@item1.unit_price_dollars).to eq('33.30')
       end
+    end
+  end
+
+  describe "best day" do
+    before :each do
+      @merch = create(:merchant)
+      @items = create_list(:item, 7, merchant: @merch)
+      @customer = create(:customer)
+      @inv0 = create(:invoice, customer: @customer, created_at: "2014-05-10 14:53:59 UTC")
+      @inv1 = create(:invoice, customer: @customer, created_at: "2016-03-27 14:53:59 UTC")
+      @inv2 = create(:invoice, customer: @customer, created_at: "2012-01-06 14:53:59 UTC")
+      @inv3 = create(:invoice, customer: @customer, created_at: "2012-03-27 14:53:59 UTC")
+      @inv4 = create(:invoice, customer: @customer, created_at: "2010-08-03 14:53:59 UTC")
+      @ii_1 = create(:invoice_item, invoice: @inv0, item: @items[1], quantity: 2, unit_price: 100)
+      @ii_2 = create(:invoice_item, invoice: @inv0, item: @items[2], quantity: 10, unit_price: 200)
+      @ii_3 = create(:invoice_item, invoice: @inv1, item: @items[3], quantity: 4, unit_price: 40)
+      @ii_4 = create(:invoice_item, invoice: @inv1, item: @items[4], quantity: 12, unit_price: 30)
+      @ii_5 = create(:invoice_item, invoice: @inv1, item: @items[5], quantity: 20, unit_price: 90)
+      @ii_6 = create(:invoice_item, invoice: @inv2, item: @items[6], quantity: 3, unit_price: 50)
+      @ii_7 = create(:invoice_item, invoice: @inv2, item: @items[1], quantity: 6, unit_price: 80)
+      @ii_8 = create(:invoice_item, invoice: @inv3, item: @items[2], quantity: 10, unit_price: 75)
+      @ii_9 = create(:invoice_item, invoice: @inv3, item: @items[2], quantity: 7, unit_price: 60)
+      @ii_10 = create(:invoice_item, invoice: @inv4, item: @items[0], quantity: 1000, unit_price: 800)
+      @trans1 = create(:transaction, invoice: @inv0)
+      @trans2 = create(:transaction, invoice: @inv1)
+      @trans3 = create(:transaction, invoice: @inv2)
+      @trans4 = create(:transaction, invoice: @inv3)
+      @trans5 = create(:failed_transaction, invoice: @inv3)
+      @trans6 = create(:failed_transaction, invoice: @inv4)
+    end
+
+    it "returns an item's best day" do
+      expect(@items[1].best_day).to eq(@inv2.created_at)
+      expect(@items[2].best_day).to eq(@inv0.created_at)
+      expect(@items[3].best_day).to eq(@inv1.created_at)
+      expect(@items[4].best_day).to eq(@inv1.created_at)
+      expect(@items[5].best_day).to eq(@inv1.created_at)
+      expect(@items[6].best_day).to eq(@inv2.created_at)
     end
   end
 end
